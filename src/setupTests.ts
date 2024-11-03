294a3d30b83a096751405b4e49dc562e71618ac7
wktaylor <wktaylorla@gmail.com>
2024-11-03 21:01:50 +0900

    🩹 일관성 있는 테스트를 위해 시간과 날짜를 고정

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -16,7 +16,7 @@ beforeAll(() => {
 beforeEach(() => {
   expect.hasAssertions();
 
-  vi.setSystemTime(new Date('2024-10-01')); // ? Medium: 왜 이 시간을 설정해주는 걸까요?
+  vi.setSystemTime(new Date('2024-10-01')); // ? Medium: 왜 이 시간을 설정해주는 걸까요? > 테스트 시간을 고정하여 일관성 있는 테스트를 위함
 });
 
 afterEach(() => {
